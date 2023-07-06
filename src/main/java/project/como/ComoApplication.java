6533dffa8edce5228ed89f1e2777b5c80ad3dd3e
its-sky <smc9919@naver.com>
2023-07-06 16:35:58 +0900

    init: 스프링부트 기본 설정

---
+++ src/main/java/project/como/ComoApplication.java
@@ -0,0 +1,13 @@
+package project.como;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/como/ComoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.como;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
