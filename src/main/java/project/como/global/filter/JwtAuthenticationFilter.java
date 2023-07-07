1cb26e9d6acf3e986b160b048088eb32b0f28317
its-sky <smc9919@naver.com>
2023-07-07 19:10:19 +0900

    feat: refreshToken Redis 저장 기능 및 refreshToken reissue 기능 추가

--- src/main/java/project/como/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/project/como/global/filter/JwtAuthenticationFilter.java
@@ -21,20 +21,19 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		String token = resolveToken((HttpServletRequest) request);
+		String token = jwtProvider.resolveToken((HttpServletRequest) request);
 
 		if (token != null && jwtProvider.validateToken(token)) {
-			Authentication authentication = jwtProvider.getAuthentication(token);
-			SecurityContextHolder.getContext().setAuthentication(authentication);
+			/*
+			 * 토큰 재발급 요청 시 refresh token을 요청 header에 넣어 Authorization 키로 받는다면,
+			 * refresh token은 내부적으로 권한 정보가 없기 때문에 getAuthentication 부분에서 Exception이 발생하므로
+			 * 한번 필터링을 해줘야 한다.
+			 */
+			if (!((HttpServletRequest) request).getRequestURI().equals("/user/reissue")) {
+				Authentication authentication = jwtProvider.getAuthentication(token);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
 		}
 		chain.doFilter(request, response);
-	}
-
-	private String resolveToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader("Authorization");
-
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer"))
-			return bearerToken.substring(7);
-		return null;
 	}
 }
