419c8158e7b19d316fe73f1d48dae7b2e89e934e
its-sky <smc9919@naver.com>
2023-07-08 16:18:37 +0900

    chore: add gitignore

--- src/main/java/project/como/domain/post/model/Post.java
+++ src/main/java/project/como/domain/post/model/Post.java
@@ -1,2 +1,27 @@
-package project.como.domain.post.model;public class Post {
+package project.como.domain.post.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Value;
+import project.como.domain.user.model.User;
+
+@Entity
+public class Post {
+
+	@Id @Column(name = "post_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@NotBlank
+	private String title;
+
+	@NotBlank
+	private String body;
+
+	private Long readCount;
+
+	private String status;
 }
