8e0ec41df362857124f48df85e9982ffb4dafa32
Kim Seongjin <whffkaos007@naver.com>
2023-07-19 17:28:17 +0900

    feat:댓글 API 개발 완료

--- src/main/java/project/como/domain/comment/service/CommentService.java
+++ src/main/java/project/como/domain/comment/service/CommentService.java
@@ -1,16 +1,20 @@
 package project.como.domain.comment.service;
 
-import project.como.domain.comment.dto.CreateCommentRequest;
+import project.como.domain.comment.dto.CommentCreateRequestDto;
+import project.como.domain.comment.dto.CommentDto;
+import project.como.domain.comment.dto.CommentUpdateDto;
 import project.como.domain.comment.model.Comment;
 
 import java.util.List;
 
 public interface CommentService {
-    public void writeComment(String userId, Long postId, CreateCommentRequest dto); //댓글 생성
-    public Comment updateComment(Long commentId, String body) throws Exception; // 댓글 수정
-    public List<Comment> findComments(); // 게시물에 작성한 모든 댓글
+    public void writeComment(String userId, Long postId, CommentCreateRequestDto dto); //댓글 생성
+    public CommentDto updateComment(Long commentId, CommentUpdateDto dto) ; // 댓글 수정
+    public List<CommentDto> findComments(Long postId); // 게시물에 작성한 모든 댓글
 
-    public void deleteComment(Long commentId) throws Exception; // 댓글 삭제
+    public CommentDto findComment(Long commentId);
+
+    public void deleteComment(Long commentId) ; // 댓글 삭제
 
     //public Comment findComment(); // 대댓글을 달기 위한 용도
 }
