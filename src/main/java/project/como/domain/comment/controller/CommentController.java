9d16b679b45db46d1f26ed6a5710c5e324ff38f4
Kim Seongjin <whffkaos007@naver.com>
2023-07-22 00:44:08 +0900

    refactor : 댓글 API 반환값, URI, DTO 형식 변경

--- src/main/java/project/como/domain/comment/controller/CommentController.java
+++ src/main/java/project/como/domain/comment/controller/CommentController.java
@@ -7,15 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.como.domain.comment.dto.CommentCreateRequestDto;
-import project.como.domain.comment.dto.CommentDto;
-import project.como.domain.comment.dto.CommentUpdateDto;
-import project.como.domain.comment.model.Comment;
+import project.como.domain.comment.dto.CommentDetailDto;
+import project.como.domain.comment.dto.CommentResponseDto;
 import project.como.domain.comment.service.CommentServiceImp;
-import project.como.domain.post.dto.PostDetailResponseDto;
 import project.como.domain.post.service.PostService;
 import project.como.global.auth.model.CurrentUser;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,52 +27,45 @@ public class CommentController {
     static class Result<T> {
         //private int count;
         private T data;
-    }
+    } // 적용 안 함
 
     //게시물 댓글 생성
-    @PostMapping("/posts/{post_id}/comment")
+    @PostMapping("/post/{post_id}/comment")
     public ResponseEntity<String> createComment(@PathVariable("post_id") Long postId
             , @RequestBody @Valid CommentCreateRequestDto dto, @CurrentUser String username) { //@CurrentUser를 통해 인증된 username 가져옴
 
         commentService.writeComment(username, postId, dto);
 
-        return ResponseEntity.ok("댓글 생성");
-        /**
-         * 궁금증 : 전반적으로 Comment 필드들 중 어디까지 보여줘야 하는지 모르겠음.
-         * 해결 : 개인적으로는 postId, username을 넘기고 싶은데 postRepository-> postId를 찾는 메서드 유무를 모름.
-         *
-         * 1) 생성한 댓글을 게시물이 보여질 때, 위와 같은 방식이 아니라 ResponseEntity<CommentCreateResponseDto> 형식으로 Dto를 넘기는 것이 효율적인가?
-         * 2) CommentCreateResponseDto를 넘겨주려 하는데 필드를 post, user 대신 postId, username를 넘겨주는 것이 맞을까? 아님 이에 대한 값이 필요가 없어도 될까?
-         */
+        return ResponseEntity.ok().body("댓글 생성"); // 일관성 통일
     }
 
     //게시물 댓글 목록 조회
-    @GetMapping("/posts/{post_id}/comments")
-    public ResponseEntity<List<CommentDto>> getComments(@PathVariable("post_id") Long postId){
+    @GetMapping("/post/{post_id}/comments")
+    public ResponseEntity<CommentResponseDto> getComments(@PathVariable("post_id") Long postId){
 
-        List<CommentDto> comments = commentService.findComments(postId);
+        CommentResponseDto comments = commentService.findComments(postId);
 
-        return ResponseEntity.ok(comments);
+        return ResponseEntity.ok().body(comments);
     }
 
     //게시물 댓글 조회(대댓글을 위해?)
 
     //게시물 댓글 수정
-    @PatchMapping("/comments/{comment_id}")
-    public ResponseEntity<CommentDto> updateComment(@PathVariable("comment_id") Long commentId,
-                                                @RequestBody @Valid CommentUpdateDto dto){
-        CommentDto commentDto = commentService.updateComment(commentId, dto);
+    @PatchMapping("/comment/{comment_id}")
+    public ResponseEntity<String> updateComment(@CurrentUser String username, //수정 권환 인증
+                                                                  @PathVariable("comment_id") Long commentId,
+                                                                  @RequestBody @Valid CommentDetailDto dto){
+        commentService.updateComment(username, commentId, dto);
 
-        return ResponseEntity.ok(commentDto);
+        return ResponseEntity.ok().body("댓글 수정");
     }
 
     //게시물 댓글 삭제
-    @DeleteMapping("/comments/{comment_id}")
+    @DeleteMapping("/comment/{comment_id}")
     public ResponseEntity<String> deleteComment(@PathVariable("comment_id") Long commentId){
         commentService.deleteComment(commentId);
 
-        return ResponseEntity.ok("id : " + commentId.toString() +", 댓글 삭제");
+        return ResponseEntity.ok().body("id : " + commentId.toString() +", 댓글 삭제");
     }
-
 
 }
--- src/main/java/project/como/domain/comment/service/CommentServiceImp.java
+++ src/main/java/project/como/domain/comment/service/CommentServiceImp.java
@@ -2,21 +2,20 @@ package project.como.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.como.domain.comment.dto.CommentCreateRequestDto;
-import project.como.domain.comment.dto.CommentDto;
-import project.como.domain.comment.dto.CommentUpdateDto;
+import project.como.domain.comment.dto.CommentDetailDto;
+import project.como.domain.comment.dto.CommentResponseDto;
 import project.como.domain.comment.model.Comment;
 import project.como.domain.comment.reposiory.CommentRepository;
+import project.como.domain.post.exception.PostNotFoundException;
 import project.como.domain.post.model.Post;
 import project.como.domain.post.repository.PostRepository;
 import project.como.domain.user.model.User;
 import project.como.domain.user.repository.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,7 +24,6 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class CommentServiceImp implements CommentService
 {
-
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
@@ -34,49 +32,53 @@ public class CommentServiceImp implements CommentService
     @Override
     public void writeComment(String username, Long postId, CommentCreateRequestDto dto) {
         User findUser = userRepository.findByUsername(username).orElseThrow(() ->
-                new IllegalArgumentException("댓글 쓰기 실패: 해당 회원이 존재하지 않습니다." + username));
+                new IllegalArgumentException("존재하지 않는 유저입니다."));
         Post findPost = postRepository.findById(postId).orElseThrow(() ->
-                new IllegalArgumentException("댓글 쓰기 실패: 해당 게시글이 존재하지 않습니다." + postId));
-
+                new PostNotFoundException(postId));
 
         Comment comment = dto.toEntity(findUser, findPost);
         commentRepository.save(comment);
     }
 
     @Override
-    public CommentDto findComment(Long commentId) {
+    public CommentDetailDto findComment(Long commentId) {
         Comment findComment = commentRepository.findById(commentId).orElseThrow(() ->
-                new IllegalArgumentException("댓글 쓰기 실패: 해당 회원이 존재하지 않습니다." + commentId));
+                new IllegalArgumentException("존재하지 않는 댓글입니다."));
 
-        return new CommentDto(findComment);
+        return CommentDetailDto.builder()
+                .body(findComment.getBody())
+                .build();
     }
 
     @Override
-    public List<CommentDto> findComments(Long postId) {
-        List<CommentDto> comments = commentRepository.findAllByPostId(postId).stream()
-                .map(c -> new CommentDto(c))
-                .collect(Collectors.toList());
-        return comments;
+    public CommentResponseDto findComments(Long postId) {
+        List<Comment> comments = commentRepository.findAllByPostId(postId);
+
+        return  CommentResponseDto.builder()
+                .comments(comments
+                            .stream()
+                            .map(c -> new CommentDetailDto(c))
+                            .collect(Collectors.toList()))
+                            .build();
     }
 
     @Transactional
     @Override
-    public CommentDto updateComment(Long commentId, CommentUpdateDto dto)  {
+    public void updateComment(String username, Long commentId, CommentDetailDto dto)  {
+        userRepository.findByUsername(username).orElseThrow(()->
+                new IllegalArgumentException("존재하지 않는 유저입니다"));
         Comment findComment = commentRepository.findById(commentId).orElseThrow(() ->
-                new IllegalArgumentException("댓글 쓰기 실패: 해당 회원이 존재하지 않습니다." + commentId));
+                new IllegalArgumentException("존재하지 않는 댓글입니다."));
 
         findComment.updateBody(dto.getBody());
-        CommentDto commentDto = new CommentDto(findComment);
-        //시간 변경 로직 추가 필요
-        return commentDto;
     }
 
     @Transactional
     @Override
     public void deleteComment(Long commentId) {
-
         Comment findComment = commentRepository.findById(commentId).orElseThrow(() ->
-                new IllegalArgumentException("댓글 쓰기 실패: 해당 회원이 존재하지 않습니다." + commentId));
+                new IllegalArgumentException("존재하지 않는 댓글입니다."));
+
         commentRepository.delete(findComment);
     }
 
