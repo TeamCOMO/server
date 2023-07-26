4c955268762df144db8625525dd97d125633aad5
its-sky <smc9919@naver.com>
2023-07-26 19:11:21 +0900

    feat: 좋아요, 좋아요 취소 API 개발

---
+++ src/main/java/project/como/domain/heart/exception/HeartNotFoundException.java
@@ -0,0 +1,8 @@
+package project.como.domain.heart.exception;
+
+import project.como.global.common.exception.ComoException;
+import project.como.global.common.exception.ErrorType;
+
+public class HeartNotFoundException extends ComoException.NotFoundException {
+	public HeartNotFoundException() { super(ErrorType.NotFound.HEART_NOT_FOUND, "하트를 누르지 않아 취소할 수 없습니다."); }
+}
