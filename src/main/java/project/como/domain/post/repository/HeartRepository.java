2b940b2b0ebc4293f98eb73f8f65b159cf8dbc1a
its-sky <smc9919@naver.com>
2023-07-30 18:17:04 +0900

    refactor: heart 구조 변경 자체 controller, service 없애고 post에 종속된 기능이라 판단하여 Post controller, service에 병합

--- src/main/java/project/como/domain/post/repository/HeartRepository.java
+++ src/main/java/project/como/domain/post/repository/HeartRepository.java
@@ -1,7 +1,7 @@
-package project.como.domain.heart.repository;
+package project.como.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import project.como.domain.heart.model.Heart;
+import project.como.domain.post.model.Heart;
 import project.como.domain.post.model.Post;
 import project.como.domain.user.model.User;
 
