538d8b0d8db76078419b7698f5157114d3a13a4f
its-sky <smc9919@naver.com>
2023-08-24 13:43:16 +0900

    remove: unneeeded files

--- src/main/java/project/como/ComoApplication.java
+++ src/main/java/project/como/ComoApplication.java
@@ -1,7 +1,10 @@
 package project.como;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,6 +13,11 @@ public class ComoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComoApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory queryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
