52875fdcd0a56153d06095af8cb0a839f5b979d8
its-sky <smc9919@naver.com>
2023-08-26 19:00:54 +0900

    test: 회원가입 테스트

---
+++ src/test/java/project/como/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,58 @@
+package project.como.domain.user.controller;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import project.como.domain.user.dto.MemberLoginRequestDto;
+import project.como.domain.user.dto.MemberSignupRequestDto;
+import project.como.domain.user.model.User;
+import project.como.domain.user.service.UserServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+	@Autowired
+	UserServiceImpl userServiceImpl;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Test
+	@DisplayName("회원가입")
+	void signUp() throws Exception {
+		MemberSignupRequestDto dto = new MemberSignupRequestDto(
+				"test",
+				"test1234",
+				"test1234",
+				"tester",
+				"test@google.com",
+				"",
+				"");
+
+		userServiceImpl.signUp(dto);
+
+		User user = em.find(User.class, 1);
+		assertThat(user.getUsername()).isEqualTo("test");
+		assertThat(user.getEmail()).isEqualTo("test@google.com");
+	}
+
+	@Test
+	void signIn() {
+	}
+
+	@Test
+	void checkDuplicate() {
+	}
+
+	@Test
+	void ping() {
+	}
+}
