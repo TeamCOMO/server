21444fa6832e80461268a1c1e48fac7744d7a8a0
its-sky <smc9919@naver.com>
2023-08-28 21:24:15 +0900

    test: 게시물 단건 조회 및 카테고리 별 페이징 조회

--- src/test/java/project/como/domain/post/controller/PostControllerTest.java
+++ src/test/java/project/como/domain/post/controller/PostControllerTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import project.como.domain.post.dto.PostCreateRequestDto;
+import project.como.domain.post.dto.PostDetailResponseDto;
+import project.como.domain.post.dto.PostsResponseDto;
 import project.como.domain.post.model.Category;
 import project.como.domain.post.model.Post;
 import project.como.domain.post.model.Tech;
@@ -15,6 +19,7 @@ import project.como.domain.post.service.PostService;
 import project.como.domain.user.model.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,10 +58,28 @@ class PostControllerTest {
 
 	@Test
 	void getDetailPost() {
+		final Long POST_ID = 1L;
+
+		Post findPost = em.find(Post.class, POST_ID);
+
+		PostDetailResponseDto servicePostDto = postService.getDetailPost(POST_ID);
+
+		assertThat(findPost.getTitle()).isEqualTo(servicePostDto.getTitle());
+		assertThat(findPost.getBody()).isEqualTo(servicePostDto.getBody());
+		assertThat(findPost.getCategory()).isEqualTo(servicePostDto.getCategory());
+		assertThat(findPost.getTechs()).containsExactly(Tech.Java, Tech.Spring);
+		assertThat(servicePostDto.getTechs()).containsExactly(Tech.Java, Tech.Spring);
 	}
 
 	@Test
 	void getPostsByCategory() {
+		final String CATEGORY = "Study";
+		Pageable pageable = PageRequest.of(0, 5);
+
+		PostsResponseDto dto = postService.getPostsByCategory(pageable, 0, CATEGORY);
+
+		assertThat(dto.getTotalElements()).isEqualTo(3);
+		assertThat(dto.getPosts().get(0).getTitle()).isEqualTo("test title");
 	}
 
 	@Test
