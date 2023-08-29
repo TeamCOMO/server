b86da786416d71d6b7daa04454c8d40d0c0ec9ce
its-sky <smc9919@naver.com>
2023-08-29 15:22:46 +0900

    feat: Post 로깅 적용

--- src/main/java/project/como/domain/post/controller/PostController.java
+++ src/main/java/project/como/domain/post/controller/PostController.java
@@ -13,6 +13,7 @@ import project.como.domain.post.dto.PostModifyRequestDto;
 import project.como.domain.post.dto.PostsResponseDto;
 import project.como.domain.post.service.PostService;
 import project.como.global.auth.model.CurrentUser;
+import project.como.global.common.model.Logging;
 
 @Slf4j
 @Transactional
@@ -23,6 +24,7 @@ public class PostController {
 
 	private final PostService postService;
 
+	@Logging(item = "Post", action = "post")
 	@PostMapping("/post/create")
 	public ResponseEntity<String> createPost(@CurrentUser String username, @RequestBody @Valid PostCreateRequestDto dto) {
 		postService.createPost(username, dto);
@@ -30,6 +32,7 @@ public class PostController {
 		return ResponseEntity.ok().body("success");
 	}
 
+	@Logging(item = "Post", action = "get")
 	@GetMapping("/post/{post_id}")
 	public ResponseEntity<PostDetailResponseDto> getDetailPost(@PathVariable(value = "post_id", required = true) Long postId) {
 		PostDetailResponseDto dto = postService.getDetailPost(postId);
@@ -37,6 +40,7 @@ public class PostController {
 		return ResponseEntity.ok().body(dto);
 	}
 
+	@Logging(item = "Post", action = "get")
 	@GetMapping("/posts/{category}")
 	public ResponseEntity<PostsResponseDto> getPostsByCategory(@PathVariable String category,
 			@RequestParam(required = false, defaultValue = "0", value = "page") int pageNo,
@@ -47,6 +51,7 @@ public class PostController {
 		return ResponseEntity.ok().body(dto);
 	}
 
+	@Logging(item = "Post", action = "patch")
 	@PatchMapping("/post/modify")
 	public ResponseEntity<String> modifyPost(@CurrentUser String username, @RequestBody PostModifyRequestDto dto) {
 		postService.modifyPost(username, dto);
@@ -54,6 +59,7 @@ public class PostController {
 		return ResponseEntity.ok().body("success");
 	}
 
+	@Logging(item = "Post", action = "delete")
 	@DeleteMapping("/post/delete/{post_id}")
 	public ResponseEntity<String> deletePost(@CurrentUser String username, @PathVariable(value = "post_id", required = true) Long postId) {
 		postService.deletePost(username, postId);
@@ -61,6 +67,7 @@ public class PostController {
 		return ResponseEntity.ok().body("success");
 	}
 
+	@Logging(item = "Post", action = "post")
 	@PostMapping("/post/heart/{post_id}")
 	public ResponseEntity<String> makeHeart(@CurrentUser String username, @PathVariable(value = "post_id", required = true) Long postId) {
 		postService.makeHeart(username, postId);
@@ -68,6 +75,7 @@ public class PostController {
 		return ResponseEntity.ok().body("success");
 	}
 
+	@Logging(item = "Post", action = "delete")
 	@DeleteMapping("/post/heart/delete/{post_id}")
 	public ResponseEntity<String> deleteHeart(@CurrentUser String username, @PathVariable(value = "post_id", required = true) Long postId) {
 		postService.deleteHeart(username, postId);
