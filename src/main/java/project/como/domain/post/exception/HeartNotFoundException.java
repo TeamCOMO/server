c398cb05ba04ff3835caf4a91c50aa7b3bd3a6f8
seungjin <whffkaos007@naver.com>
2023-08-30 16:15:22 +0900

    refactor : 댓글 수정, 삭제 예외처리 추가

--- src/main/java/project/como/domain/post/exception/HeartNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package project.como.domain.post.exception;
-
-import project.como.global.common.exception.ComoException;
-import project.como.global.common.exception.ErrorType;
-
-public class HeartNotFoundException extends ComoException.NotFoundException {
-	public HeartNotFoundException() { super(ErrorType.NotFound.HEART_NOT_FOUND, "하트를 누르지 않아 취소할 수 없습니다."); }
-}
--- src/main/java/project/como/global/auth/repository/RefreshTokenRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package project.como.global.auth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import project.como.global.auth.model.RefreshToken;
-
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
-	RefreshToken findByRefreshToken(String refreshToken);
-}
