09505810c0c78bb850d6f82370dd4ae54520c047
its-sky <smc9919@naver.com>
2023-09-01 15:18:35 +0900

    feat: PostAccessDeniedException 핸들링 및 반환

--- src/main/java/project/como/global/common/exception/ComoControllerAdvice.java
+++ src/main/java/project/como/global/common/exception/ComoControllerAdvice.java
@@ -6,21 +6,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.como.domain.post.exception.PostAccessDeniedException;
 import project.como.domain.post.exception.PostNotFoundException;
 import project.como.global.auth.exception.ComoLoginFailureException;
 import project.como.global.common.dto.ErrorResponse;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
 @RestControllerAdvice
 public class ComoControllerAdvice {
 
-	@ExceptionHandler(value = PostNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handlePostNotFoundException(PostNotFoundException ex) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(new PostNotFoundException()));
-	}
-
 	@ExceptionHandler(value = BadCredentialsException.class)
 	public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(new ComoLoginFailureException()));
+		return ResponseEntity.status(UNAUTHORIZED).body(new ErrorResponse(new ComoLoginFailureException()));
+	}
+
+	@ExceptionHandler(value = PostNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handlePostNotFoundException(PostNotFoundException ex) {
+		return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse(new PostNotFoundException()));
+	}
+
+	@ExceptionHandler(value = PostAccessDeniedException.class)
+	public ResponseEntity<ErrorResponse> handlePostAccessDeniedException(PostAccessDeniedException ex) {
+		return ResponseEntity.status(FORBIDDEN).body(new ErrorResponse(new PostAccessDeniedException()));
 	}
 }
