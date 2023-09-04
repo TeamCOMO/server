b45cfaa3a8b48547002cf98f9c8275bd6b0ab98f
its-sky <smc9919@naver.com>
2023-09-04 16:22:12 +0900

    refactor: 비밀번호 정규식 특수문자 허용

--- src/main/java/project/como/domain/user/dto/MemberSignupRequestDto.java
+++ src/main/java/project/como/domain/user/dto/MemberSignupRequestDto.java
@@ -22,7 +22,7 @@ public class MemberSignupRequestDto {
 	private String username;
 
 	@NotBlank(message = "비밀번호를 입력해주세요.")
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,30}$",
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()_+|<>?:{}])[A-Za-z\\d~!@#$%^&*()_+|<>?:{}]{8,30}$",
 			message = "비밀번호는 8~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
 	private String password;
 
