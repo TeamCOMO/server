1a431b2ba625835149ddf30364dfd29cd6d42c02
its-sky <smc9919@naver.com>
2023-11-08 17:55:40 +0900

    refactor: Post 페이징 조회 조건 변경(검색 조건에 여러개의 Tech를 선택할 수 있는데 이 여러가지 Tech 중 하나라도 포함된다면 조건에 포함하여 반환)

--- src/main/java/project/como/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/project/como/domain/post/repository/PostCustomRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,8 @@ import project.como.domain.image.model.Image;
 import project.como.domain.post.dto.*;
 import project.como.domain.post.model.Category;
 import project.como.domain.post.model.Post;
+import project.como.domain.post.model.QPost;
+import project.como.domain.post.model.QPostTech;
 import project.como.domain.post.model.Tech;
 
 import java.util.List;
@@ -22,6 +25,7 @@ import java.util.stream.Collectors;
 
 import static project.como.domain.image.model.QImage.*;
 import static project.como.domain.post.model.QPost.*;
+import static project.como.domain.post.model.QPostTech.*;
 import static project.como.domain.post.model.QTech.*;
 
 @Slf4j
@@ -32,7 +36,7 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
 
 	public Page<PostPagingResponseDto> findAllByCategoryAndTechs(Category category, List<String> stacks, Pageable pageable) {
 		List<Post> tmp_posts = queryFactory.selectFrom(post)
-				.join(post.techs).fetchJoin()
+				.join(post.techList, postTech).fetchJoin()
 				.where(categoryEq(category)
 						.and(containsTechs(stacks)))
 				.orderBy(post.createdDate.desc())
@@ -46,7 +50,7 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
 			dto.setTitle(post.getTitle());
 			dto.setCategory(post.getCategory());
 			dto.setState(post.getState());
-			dto.setTechs(post.getTechs().stream().map(Tech::getStack).collect(Collectors.toList()));
+			dto.setTechs(post.getTechList().stream().map((pt) -> pt.getTech().getStack()).toList());
 			dto.setHeartCount(post.getHeartCount());
 			return dto;
 		}).toList();
@@ -63,7 +67,7 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
 
 	public PostDetailResponseDto findPostDetailById(Long id) {
 		Post result = queryFactory.selectFrom(post)
-				.join(post.techs, tech).fetchJoin()
+				.join(post.techList, postTech).fetchJoin()
 				.leftJoin(post.images, image)
 				.where(post.id.eq(id))
 				.fetchOne();
@@ -74,7 +78,7 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
 				.body(result.getBody())
 				.category(result.getCategory())
 				.state(result.getState())
-				.techs(result.getTechs().stream().map(Tech::getStack).distinct().collect(Collectors.toList()))
+				.techs(result.getTechList().stream().distinct().map((t) -> t.getTech().getStack()).sorted().toList())
 				.images(result.getImages().stream().map(Image::getUrl).collect(Collectors.toList()))
 				.heartCount(result.getHeartCount())
 				.build();
@@ -84,20 +88,37 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
 		return category != null ? post.category.eq(category) : null;
 	}
 
-	private BooleanBuilder containsTechs(List<String> stacks) {
-		BooleanBuilder builder = new BooleanBuilder();
+	private BooleanExpression containsTechs(List<String> stacks) {
+		BooleanExpression where = null;
+		if (stacks == null || stacks.isEmpty()) return null;
 
-		if (stacks == null || stacks.isEmpty()) return builder;
-		else {
-			builder.and(
-					post.in(
-							JPAExpressions.select(tech.post)
-									.from(tech)
-									.where(tech.stack.in(stacks))
-					)
-			);
+		for (String stack : stacks) {
+			BooleanExpression exp = post.techList.any().tech.stack.eq(stack);
+			where = (where == null) ? exp : where.or(exp);
 		}
 
-		return builder;
+		return where;
+//		else {
+//			builder.and(
+//
+//					post.techList.contains(JPAExpressions.
+//							selectFrom(postTech)
+//							.where(postTech.tech.stack.in(stacks)))
+//			);
+//			for (String tech : stacks) {
+//				builder.and(post.techList.contains(
+//						JPAExpressions.selectFrom(postTech)
+//								.where(postTech.tech.stack.eq(tech))
+//				));
+////				builder.and(post.techList.in(
+////						JPAExpressions
+////								.select(postTech.tech.stack)
+////								.from(post)
+////								.join(post.techList, postTech)
+////								.where(postTech.tech.stack.eq(tech))
+////				));
+//			}
+		// 하나라도 포함되면 Post의 조건에 맞도록 해야함.
+		// 예를 들어, Java, Spring 을 골랐을 때 둘 다 있는 Post만 제시하는 것이 아니라 Java만 있는 게시물과 Spring만 있는 게시물, 둘 다 포함하는 게사물 모두 제시해야함.
 	}
 }
