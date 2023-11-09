35d6f866821bcf12397b9a673c0c9773d4a649c6
its-sky <smc9919@naver.com>
2023-11-09 16:25:34 +0900

    feat: 유저 정보 수정 API 구현 및 UserService PSA 이용하여 의존성 주입

---
+++ src/main/java/project/como/domain/user/dto/MemberModifyRequestDto.java
@@ -0,0 +1,23 @@
+package project.como.domain.user.dto;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+
+public record MemberModifyRequestDto(
+        @Nullable
+        @URL
+        String blog_url,
+        @Nullable
+        @URL
+        String github_url,
+        @Nullable
+        @Size(min = 2, message = "닉네임은 최소 2자 이상으로 입력해주세요.")
+        String nickname,
+        @Nullable
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()_+|<>?:{}])[A-Za-z\\d~!@#$%^&*()_+|<>?:{}]{8,30}$",
+                message = "비밀번호는 8~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
+        String password
+) {
+}
