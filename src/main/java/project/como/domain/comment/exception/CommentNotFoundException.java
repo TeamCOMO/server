4a61557be7d95980227167f24b08dab971654c00
Kimseungin0529 <whffkaos007@naver.com>
2023-11-14 16:54:11 +0900

    refactor : CommentNotFoundException 메세지 수정
    잘못된 메세지 값 수정, 불필요한 코드 삭제

--- src/main/java/project/como/domain/comment/exception/CommentNotFoundException.java
+++ src/main/java/project/como/domain/comment/exception/CommentNotFoundException.java
@@ -5,11 +5,7 @@ import project.como.global.common.exception.ErrorType;
 
 public class CommentNotFoundException  extends ComoException.NotFoundException {
 
-	public CommentNotFoundException() {
-		super(ErrorType.NotFound.COMMENT_NOT_FOUND, "해당 게시글을 찾을 수 없습니다.");
-	}
-
 	public CommentNotFoundException(Long commentId) {
-		super(ErrorType.NotFound.COMMENT_NOT_FOUND, "ID가 " + commentId + "인 게시글을 찾을 수 없습니다.");
+		super(ErrorType.NotFound.COMMENT_NOT_FOUND, "ID가 " + commentId + "인 댓글을 찾을 수 없습니다.");
 	}
 }
